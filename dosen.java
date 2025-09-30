941419bf3e3f2362cd8465aa32e8f61b0b71f083
Denzel D <denzeldaniels123@gmail.com>
2025-09-30 20:13:06 +0700

    dosen.java

---
+++ dosen.java
@@ -0,0 +1,22 @@
+
+public class Dosen {
+    private String nama;
+    private String nidn;
+
+    public Dosen(String nama, String nidn) {
+        this.nama = nama;
+        this.nidn = nidn;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNidn() {
+        return nidn;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Dosen: " + nama + " | NIDN: " + nidn);
+    }
+}
